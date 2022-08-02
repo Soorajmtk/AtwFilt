24942fbe5445d792fc2e6ec66308ebeb7a909d11
Soorajmtk <108129312+Soorajmtk@users.noreply.github.com>
2022-08-02 19:58:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/illuzX/AtwFilt /AtwFilt
+  git clone https://github.com/Soorajmtk
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
